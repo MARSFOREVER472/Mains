a825574ed56b03a3490a3b101a35ea800dd3db26
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-06-18 18:53:42 -0400

    Se agregaron más variables!

--- Archivos/index.js
+++ Archivos/index.js
@@ -1,1 +1,11 @@
 console.log("Hello world!!!!!");
+
+var numero1 = 52.5;
+var sentencia2 = "ESTO ES UN TEXTO";
+var sentencia3 = false;
+var sentencia4 = true;
+
+console.log(numero1);
+console.log(sentencia2);
+console.log(sentencia3);
+console.log(sentencia4);
